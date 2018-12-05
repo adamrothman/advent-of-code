c6761b04d17a2d79ed5bad77a9cbb9e5526b213b
Adam Rothman <rothman.adam@gmail.com>
2018-12-05 01:06:10 -0800

    WIP solution for day 5

--- 2018/day05/main.go
+++ 2018/day05/main.go
@@ -1,3 +1,69 @@
 package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func readInput(filename string) (string, error) {
+	path, err := filepath.Abs(filename)
+	if err != nil {
+		return "", fmt.Errorf("constructing absolute path from %s: %s", filename, err)
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		return "", fmt.Errorf("opening config file %s for reading: %s", path, err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	var text string
+
+	for scanner.Scan() {
+		text = scanner.Text()
+		break
+	}
+	if err := scanner.Err(); err != nil {
+		return "", fmt.Errorf("reading input file: %s", err)
+	}
+
+	return text, nil
+}
+
+func react(polymer string) string {
+	for {
+		reacted := polymer
+		for i, j := 0, 1; j < len(polymer); i, j = i+1, j+1 {
+			one, two := int32(reacted[i]), int32(reacted[j])
+			if abs32(one-two) == 32 {
+				fmt.Printf("reaction between %c and %c\n", one, two)
+			}
+		}
+
+		if reacted == polymer {
+			return reacted
+		}
+		polymer = reacted
+	}
+}
+
+func abs32(x int32) int32 {
+	y := x >> 31
+	return (x ^ y) - y
+}
+
+func main() {
+	filename := "input.txt"
+
+	polymer, err := readInput(filename)
+	if err != nil {
+		log.Printf("Error reading input from %s: %s\n", filename, err)
+	}
+
+	result := react(polymer)
+	fmt.Println("Reaction result:", result)
+}
